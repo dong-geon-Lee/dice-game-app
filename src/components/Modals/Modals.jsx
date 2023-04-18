572ff455f7ebff97e32499a76d5bbfbde3636e20
dong-geon-Lee <ch9716@naver.com>
2023-04-18 17:42:20 +0900

    Modals & Overlays 컴포넌트 스타일 및 상수 리팩토링

--- src/components/Modals/Modals.jsx
+++ src/components/Modals/Modals.jsx
@@ -1,35 +1,28 @@
 import React from "react";
 import { useRecoilState } from "recoil";
-import { closeModals } from "../../helpers/helpers";
 import { modalState, overlayState } from "../../recoils/modalState";
-import { Container, Button, Title, Text, Strong } from "./styles";
-import {
-  CLOSE__BTN,
-  GAME__RULES__FIVE,
-  GAME__RULES__FOUR,
-  GAME__RULES__ONE,
-  GAME__RULES__THREE,
-  GAME__RULES__TWO,
-} from "../../constants/constants";
+import { closeModals } from "../../helpers/helpers";
+import * as S from "./styles";
+import * as C from "../../constants/constants";
 
 const Modals = () => {
   const [, setModals] = useRecoilState(modalState);
   const [, setOverlays] = useRecoilState(overlayState);
 
   return (
-    <Container>
-      <Button onClick={() => closeModals(setModals, setOverlays)}>
-        {CLOSE__BTN}
-      </Button>
-      <Title>
-        <Strong>게임목표</Strong> : <Strong>25</Strong>점을 획득하세요!
-      </Title>
-      <Text>{GAME__RULES__ONE}</Text>
-      <Text>{GAME__RULES__TWO}</Text>
-      <Text>{GAME__RULES__THREE}</Text>
-      <Text>{GAME__RULES__FOUR}</Text>
-      <Text>{GAME__RULES__FIVE}</Text>
-    </Container>
+    <S.Container>
+      <S.Button onClick={() => closeModals(setModals, setOverlays)}>
+        {C.CLOSE__BTN}
+      </S.Button>
+      <S.Title>
+        <S.Strong>게임목표</S.Strong> : <S.Strong>25</S.Strong>점을 획득하세요!
+      </S.Title>
+      <S.Text>{C.GAME__RULES__ONE}</S.Text>
+      <S.Text>{C.GAME__RULES__TWO}</S.Text>
+      <S.Text>{C.GAME__RULES__THREE}</S.Text>
+      <S.Text>{C.GAME__RULES__FOUR}</S.Text>
+      <S.Text>{C.GAME__RULES__FIVE}</S.Text>
+    </S.Container>
   );
 };
 
--- src/components/Overlays/Overlays.jsx
+++ src/components/Overlays/Overlays.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useRecoilState } from "recoil";
-import { closeModals } from "../../helpers/helpers";
 import { modalState, overlayState } from "../../recoils/modalState";
-import { Container } from "./styles";
+import { closeModals } from "../../helpers/helpers";
+import * as S from "./styles";
 
 const Overlays = () => {
   const [, setModals] = useRecoilState(modalState);
   const [, setOverlays] = useRecoilState(overlayState);
 
-  return <Container onClick={() => closeModals(setModals, setOverlays)} />;
+  return <S.Container onClick={() => closeModals(setModals, setOverlays)} />;
 };
 
 export default Overlays;
